c10b664455afaf28ddc987b266f4984ce0695592
Sam Tovey <samtovey13@gmail.com>
2021-10-11 18:01:04 +0100

    added dynamic titles to html pages and set home to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'html_pages/home'
+  root 'html_pages#home'
   get 'html_pages/help'
   get 'html_pages/about'
-  root 'application#hello'
 end
--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -4,15 +4,18 @@ class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get html_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Appraisl Feedback Application"
   end
 
   test "should get help" do
     get html_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Appraisl Feedback Application"
   end
 
   test "should get about" do
     get html_pages_about_url
     assert_response :success
+    assert_select "title", "About | Appraisl Feedback Application"
   end
 end
